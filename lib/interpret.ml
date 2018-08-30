4b6a1ff3424c7204a629a78e7acbea367436d110
ashiato45 <katiiijp@gmail.com>
2018-08-30 18:29:16 +0900

    added test

--- lib/interpret.ml
+++ lib/interpret.ml
@@ -6,7 +6,7 @@ module Environment = Map.Make(String)
 
 exception Undefined_variable
 exception Interpretation_error
-exception Invariant_violation
+exception Invariant_violation of (int Environment.t * Parse.pbool)
 
 let rec interpret_num (e:int Environment.t) (n:pnum) : int = 
   match n with
@@ -44,13 +44,13 @@ let rec interpret (e:int Environment.t) (p:prog) : int Environment.t =
     | false -> e
     | true -> (
       let e' = interpret e p in
-      if (interpret_bool e' inv) then raise Invariant_violation;
-      e'
+      if not (interpret_bool e' inv) then raise (Invariant_violation (e', inv));
+      interpret e' (PWhile (cond, inv, p))
     )
   ) 
   | PBlock [] -> e
   | PBlock (x::xs) -> (
-    let e' = interpret e p in
+    let e' = interpret e x in
     interpret e' (PBlock xs)
   )
 let%expect_test "interpret1" =
@@ -59,3 +59,9 @@ let%expect_test "interpret1" =
   let res = interpret env prog in
   print_endline (res |> [%sexp_of: int Environment.t] |> Sexp.to_string);
   [%expect {| ((x 3)) |}]
+let%expect_test "interpret2" =
+  let prog = read_prog (Sexp.of_string Testprog.whileadd) in
+  let env = Environment.of_alist_exn [("x", 3); ("y", 2); ("a", 3); ("b", 2)] in
+  let res = interpret env prog in
+  print_endline (res |> [%sexp_of: int Environment.t] |> Sexp.to_string);
+  [%expect {| ((a 3)(b 2)(x 0)(y 5)) |}]
--- lib/testprog.ml
+++ lib/testprog.ml
@@ -1,3 +1,8 @@
 let p = "(if (>= x 0)
          skip
          (set! x (- 0 x)))"
+let whileadd = "(while (> x 0)
+          (and (>= x 0) (= (+ x y) (+ a b)))
+          (block
+           (set! x (- x 1))
+          (set! y (+ y 1))))"
