a395f0293b7e113b095e2e22642e2030a46945c1
ashiato45 <katiiijp@gmail.com>
2018-09-05 14:58:06 +0900

    defined subst

--- lib/verify.ml
+++ lib/verify.ml
@@ -4,14 +4,33 @@ open Sexp
 open Testprog
 open Parse
 
-let subst_pbool (cond:pbool) (var:string) (replacing:pnum) : pbool =
-  assert false
+let rec subst_pnum (var:string) (replacing:pnum) (n:pnum) : pnum = 
+  match n with
+  | NVar v -> (
+      if [%compare: string] v var = 0 then replacing else n
+    )
+  | NPlus (l, r) -> NPlus (subst_pnum var replacing l, subst_pnum var replacing r)
+  | NMinus (l, r) -> NMinus (subst_pnum var replacing l, subst_pnum var replacing r)
+  | otherwise -> n
+                          
+
+let rec subst_pbool (var:string) (replacing:pnum) (cond:pbool) : pbool =
+  let subst_pbool' = subst_pbool var replacing in
+  let subst_pnum' = subst_pnum var replacing in
+  match cond with
+  | BEqual (l, r) -> BEqual (subst_pnum' l, subst_pnum' r)
+  | BGt (l, r) -> BGt (subst_pnum' l, subst_pnum' r)
+  | BLt (l, r) -> BLt (subst_pnum' l, subst_pnum' r)
+  | BGe (l, r) -> BGe (subst_pnum' l, subst_pnum' r)
+  | BLe (l, r) -> BLe (subst_pnum' l, subst_pnum' r)
+  | BAnd (l, r) -> BAnd (subst_pbool' l, subst_pbool' r)
+  | otherwise -> assert false
 
 let calc_weakest_precond (prog:prog) (post:pbool) : pbool*(pbool list) =
   let rec help (prog:prog) (post:pbool) (acc:pbool list) : pbool*(pbool list) =
     match prog with
     | PSkip -> (post, acc)
-    | PSet (var, n) -> (subst_pbool post var n, acc)
+    | PSet (var, n) -> (subst_pbool var n post, acc)
     | PIf (cond, t, f) -> (
       let (pos_cond, acc) = help t post acc in
       let (neg_cond, acc) = help f post acc in
